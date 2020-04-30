cea42950dd0d1cc73673269dbdac49d36ba6f969
Guruprasad Kulkarni <comdotlinux@users.noreply.github.com>
2020-04-30 16:07:46 +0200

    Fixing using uploads (#2)
    * Skipped Artifacts from release.
    
    * Update build-deb.sh
    
    * Uploading artifacts

--- build-deb.sh
+++ build-deb.sh
@@ -123,16 +123,21 @@ echo "Release ID : ${release_id}"
 [ -z release_id ] && echo "Failed to get release id" && exit 3
 echo "--"
 zip ${packageName}.deb.zip ${packageName}.deb
-uploadUrlZip="https://uploads.github.com/repos/comdotlinux/postman/releases/${release_id}/assets?name=${packageName}.deb.zip&label=ZippedDebFile"
-uploadUrl="https://uploads.github.com/repos/comdotlinux/postman/releases/${release_id}/assets?name=${packageName}.deb"
+mkdir ${release_id}
+mv -v ${packageName}.deb.zip ${release_id}
+mv -v ${packageName}.deb ${release_id}
+echo "::set-env name=PACKAGE_DIR::${release_id}"
 
-echo "Upload Url is : ${uploadUrl}"
-curl -i -L -H "Authorization: token ${GITHUB_TOKEN}" -H 'Content-Type: application/vnd.debian.binary-package' --data @${packageName}.deb ${uploadUrl}
+#uploadUrlZip="https://uploads.github.com/repos/comdotlinux/postman/releases/${release_id}/assets?name=${packageName}.deb.zip&label=ZippedDebFile"
+#uploadUrl="https://uploads.github.com/repos/comdotlinux/postman/releases/${release_id}/assets?name=${packageName}.deb"
 
-echo "Upload Url for zip is : ${uploadUrlZip}"
-curl -i -L -H "Authorization: token ${GITHUB_TOKEN}" -H 'Content-Type: application/zip' --data @${packageName}.deb.zip ${uploadUrlZip}
+#echo "Upload Url is : ${uploadUrl}"
+#curl -i -L -H "Authorization: token ${GITHUB_TOKEN}" -H 'Content-Type: application/vnd.debian.binary-package' --data @${packageName}.deb ${uploadUrl}
 
-
+#echo "Upload Url for zip is : ${uploadUrlZip}"
+#curl -i -L -H "Authorization: token ${GITHUB_TOKEN}" -H 'Content-Type: application/zip' --data @${packageName}.deb.zip ${uploadUrlZip}
+#
+#
 echo "--"
 curl -s --fail -L https://api.github.com/repos/comdotlinux/postman/releases/tags/v${version} -o /dev/null
 if [ $? -ne 0 ] ; then
